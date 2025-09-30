49757e129cfa41fcd79a443667da6fdbe9c03ecd
default <bthistle@huronperthcatholic.ca>
2025-09-30 15:24:23 +0000

    added a text displaying the courses I am taking for this semester

---
+++ program-1/main.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-09-30
+ * @fileoverview This program displays the courses I am taking
+ */
+
+//string literal text
+console.log("I am Brayden Thistle and my courses for this semester are as follows:");
+
+//string literal text
+console.log("ENG3C - English");
+
+//string literal text
+console.log("ICS3U - Computer Science");
+
+//string literal text
+console.log("TTJ3C - Transportation Technology");
+
+//string literal text
+console.log("TCE3E - Electrical Technology");
+
+//string literal text
+console.log("\nDone");
