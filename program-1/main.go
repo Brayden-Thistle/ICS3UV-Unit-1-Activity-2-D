f80d9ef272d895b27d648019e71d831e7f497b48
default <bthistle@huronperthcatholic.ca>
2025-09-30 15:35:34 +0000

    added a text to display the courses I am taking next semester

--- program-1/main.go
+++ program-1/main.go
@@ -1,1 +1,30 @@
-package program1
+/*
+* @author Brayden Thistle
+* @version 1.0.0
+* @date 2025-09-30
+* @fileoverview This program displays the courses I am taking this semester.
+*/
+
+package main
+
+import "fmt"
+
+func main () {
+	//string literal text
+	fmt.Println("Hello I am Brayden Thistle and the courses I'm taking next semester are as follows:")
+
+	//string literal text
+	fmt.Println("PAF3Oa - Fitness")
+
+	//string literal text
+	fmt.Println("HRF3Ob - Religion")
+
+	//string literal text
+	fmt.Println("MCF3Mb - Functions and Applications")
+
+	//string literal text
+	fmt.Println("AMU3Ma - Instrumental Music")
+
+	//string literal text
+	fmt.Println("\nDone")
+}
