3c0b5157b53dbf40978a5f4a141ff46727d90ec9
default <bthistle@huronperthcatholic.ca>
2025-10-01 15:09:46 +0000

    added the hail mary prayer into Go.

--- program-2/main.go
+++ program-2/main.go
@@ -1,1 +1,39 @@
-package program2
+/* 
+* @author Brayden Thistle
+* @version 1.0.0
+* @date 2025-10-01
+* @fileoverview this code displays the hail mary prayer.
+*/
+
+package main
+
+import "fmt"
+
+func main () {	
+	//string literal text
+	fmt.Println("Hail, Mary, Full of grace")
+	
+	//string literal text
+	fmt.Println("the Lord is with thee.")
+
+	//string literal text
+	fmt.Println("Blessed art though amongst women")
+
+	//string literal text
+	fmt.Println("and blessed is the fruit of thy womb, Jesus")
+
+	//string literal text
+	fmt.Println("Holy Mary, Mother of God,")
+
+	//string literal text
+	fmt.Println("pray for us sinners,")
+
+	//string literal text
+	fmt.Println("now and at the hour of our death.")
+
+	//string literal text
+	fmt.Println("Amen.")
+
+	//string literal text
+	fmt.Println("\nDone")
+}
