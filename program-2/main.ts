7c2c82b1f807011ec33b595d77cede43d95c50d5
default <bthistle@huronperthcatholic.ca>
2025-10-01 15:19:10 +0000

    added the Hail Mary prayer to TypeScript

---
+++ program-2/main.ts
@@ -0,0 +1,33 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview this code displays the hail mary prayer.
+ */
+
+//string literal text
+console.log("Hail, Mary, full of grace,");
+
+//string literal text
+console.log("the Lord is with thee");
+
+//string literal text
+console.log("Blessed art thou amongst women");
+
+//string literal text
+console.log("and blessed is the fruit of thy womb, Jesus.");
+
+//string literal text
+console.log("Holy Mary, Mother of God,");
+
+//string literal text
+console.log("pray for us sinners,");
+
+//string literal text
+console.log("now and at the hour of our death.");
+
+//string literal text
+console.log("Amen.");
+
+//string literal text
+console.log("\nDone");
