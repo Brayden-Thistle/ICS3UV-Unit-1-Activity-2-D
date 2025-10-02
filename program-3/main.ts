65490348f3f55febb91bb9b3ffc839c639ec3ebf
default <bthistle@huronperthcatholic.ca>
2025-10-02 13:06:32 +0000

    added the happy birthday song into TypeScript

---
+++ program-3/main.ts
@@ -0,0 +1,21 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-10-02
+ * @fileoverfiew This program displays happy birthday
+ */
+
+//string literal text
+console.log("Happy Birthday to you");
+
+//string literal text
+console.log("Happy birthday to you");
+
+//string literal text
+console.log("Happy birthday, dear Brayden");
+
+//string literal text
+console.log("Happy birthday to you.")
+
+//string literal text
+console.log("\nDone");
